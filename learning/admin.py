1c8a6b5a44e6591acf070c157b38865fd32664e0
Guillaume Bernard <guillaume.bernard@loria.fr>
2019-03-11 12:21:44 +0100

    feat: new course in model

--- learning/admin.py
+++ learning/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Course
+
+
+class CourseAdmin(admin.ModelAdmin):
+    model = Course
+    list_display = ('name', 'state', 'published', 'author')
+
+
+admin.site.register(Course, CourseAdmin)
--- learning/models.py
+++ learning/models.py
@@ -1,3 +1,55 @@
-from django.db import models
+from enum import Enum
 
-# Create your models here.
+from django.db import models
+from accounts.models import Person
+
+from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import pgettext_lazy
+
+
+class CourseState(Enum):
+    DRAFT = _("Draft")
+    PUBLISHED = _("Published")
+    ARCHIVED = _("Archived")
+
+
+class Course(models.Model):
+
+    name = models.CharField(
+        max_length=255,
+        verbose_name=_("Name")
+    )
+    description = models.TextField(
+        blank=True,
+        verbose_name=_("Description")
+    )
+    state = models.CharField(
+        max_length=20,
+        choices=[(state.name, state.value) for state in CourseState],
+        default=CourseState.DRAFT,
+        verbose_name=_("State")
+    )
+    published = models.DateTimeField(
+        auto_now_add=True,
+        auto_now=False,
+        verbose_name=_("Published the")
+    )
+    updated = models.DateTimeField(
+        auto_now_add=False,
+        auto_now=True,
+        verbose_name=_("Last updated the")
+    )
+    author = models.ForeignKey(
+        Person,
+        on_delete=models.CASCADE,
+        related_name="writes",
+        verbose_name=_("Author")
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
+        verbose_name = pgettext_lazy("Course verbose name (singular form)", "course")
+        verbose_name_plural = pgettext_lazy("Course verbose name (plural form)", "courses")
