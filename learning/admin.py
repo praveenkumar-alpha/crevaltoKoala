cbffe301e4bbc23877b0f2b0c71e83cb0984c45a
Guillaume Bernard <guillaume.bernard@loria.fr>
2019-03-14 09:06:37 +0100

    feat: switch admin course to use guardian

--- learning/admin.py
+++ learning/admin.py
@@ -1,16 +1,23 @@
 from django.contrib import admin
+from guardian.admin import GuardedModelAdmin
 from guardian.shortcuts import assign_perm
 
 from .models import Course
 
 
-class CourseAdmin(admin.ModelAdmin):
+class CourseAdmin(GuardedModelAdmin):
+    """
+     Inheriting from GuardedModelAdmin just adds access to per-object
+     permission management tools. This can be replaced by ModelAdmin at any
+     time.
+    """
+
     model = Course
     list_display = ('id', 'name', 'state', 'published', 'author')
     list_filter = ('published', 'state')
 
     def save_model(self, request, obj, form, change):
-        obj.save()
+        super(CourseAdmin, self).save_model(request, obj, form, change)
         assign_perm('change_course', obj.author, obj)
         assign_perm('delete_course', obj.author, obj)
 
