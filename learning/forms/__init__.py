5d69d0ee2da898b5ee980c5aa7bd6e57fc070b1e
Guillaume Bernard <guillaume.bernard@loria.fr>
2019-03-20 15:22:40 +0100

    chore: move learning away

--- learning/forms/__init__.py
+++
@@ -1,2 +0,0 @@
-from learning.forms.course import CourseCreateForm, CourseUpdateForm
-from learning.forms.activity import ActivityCreateForm, ActivityUpdateForm
--- learning/views/__init__.py
+++
@@ -1,3 +0,0 @@
-from learning.views.course import CourseCreateView, CourseUpdateView, CourseDeleteView, CourseDetailView, CourseListView
-from learning.views.activity import ActivityCreateView, activity_create_on_course_view, activity_on_course_unlink_view, activity_on_course_delete_view, ActivityListView, \
-    ActivityDetailView, ActivityUpdateView, ActivityDeleteView
