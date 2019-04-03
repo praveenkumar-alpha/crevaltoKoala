ab310a3c9c08429b182da976577b217bb3ad3a18
Guillaume Bernard <gbernard@koala-lms.org>
2019-04-03 09:55:10 +0200

    feat: run demo app using Docker

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM python:slim
+MAINTAINER Guillaume Bernard <gbernard@koala-lms.org>
+
+ENV PYTHONUNBUFFERED 1
+ENV PYTHONDONTWRITEBYTECODE 1
+
+# Let this first for caching reasons
+RUN apt update && \
+    apt install -y gettext intltool gnome-doc-utils itstool libmariadbclient-dev libicu-dev build-essential git
+
+# Implicitely creates directory
+WORKDIR /koala_lms
+COPY requirements.txt /koala_lms/requirements.txt
+
+# Install requirements
+RUN pip3 install -r requirements.txt uwsgi
+COPY .  /koala_lms/
+
+RUN chmod u+x /koala_lms/docker-entrypoint.sh
+ENTRYPOINT docker-entrypoint.sh
---
+++ docker-entrypoint.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Initialize SECRET_KEY for Django if none is already given
+local_settings="./server/local_settings.py"
+echo "ALLOWED_HOSTS = ['127.0.0.1']" > "${local_settings}"
+if ! grep 'SECRET_KEY' "${local_settings}" &> /dev/null ; then
+    echo "SECRET_KEY = 'supersecretkey'" >> "${local_settings}"
+fi
+
+if [[ ! -z "${LANGUAGE_CODE+x}" ]]; then
+    echo "LANGUAGE_CODE = '${LANGUAGE_CODE}'" >> "${local_settings}"
+fi
+
+if [[ ! -z "${TIME_ZONE+x}" ]]; then
+    echo "TIME_ZONE = '${TIME_ZONE}'" >> "${local_settings}"
+fi
+
+# Prepare database migration
+python3 ./manage.py migrate
+
+# Load sample data into database
+if [[ ! -z "${FIXTURE+x}" ]]; then
+    python3 ./manage.py loaddata "${FIXTURE}"
+else
+    echo "Fixture is not set, skipping loading fixtures…"
+fi
+
+# Compile translations
+python3 ./manage.py compilemessages
+
+# Collect static files
+python3 ./manage.py collectstatic
+
+# Run webserver
+uwsgi --ini uwsgi.ini
