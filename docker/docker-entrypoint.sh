6e979f565fb0f1b2235cf0181651bd3e845c0e81
Guillaume Bernard <gbernard@koala-lms.org>
2019-04-18 13:54:13 +0200

    feat: split base and demo docker images

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -2,20 +2,11 @@
 
 local_settings_file="./server/local_settings.py"
 echo "# This file is generated automatically, do not edit manually"
-
-# Initialize django secret key
-if ! grep 'SECRET_KEY' "${local_settings_file}" &> /dev/null ; then
-    echo "SECRET_KEY = '$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)'" >> "${local_settings_file}"
-fi
-
-# Only allow localhost, because used behind a proxy
+echo "SECRET_KEY = '$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)'" >> "${local_settings_file}"
 echo "ALLOWED_HOSTS = ['127.0.0.1', 'localhost']" >> "${local_settings_file}"
 
-# If running in demo (variable should exists, whatever it contains), install dependencies from current build
 if [[ ! -z "${DEMO+x}" ]]; then
     echo "DEMO = True" >> "${local_settings_file}"
-    curl -L "https://gitlab.com/koala-lms/django-learning/-/jobs/artifacts/master/download?job=package" -o learning.zip
-    mkdir learning && unzip -o learning.zip -d ./learning && pip install learning/dist/*.tar.gz && rm -rf learning
 fi
 
 if [[ ! -z "${LANGUAGE_CODE+x}" ]]; then
