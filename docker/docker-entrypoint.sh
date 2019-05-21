a0b5a0713295823aa572d9bbaf137a33c3432a52
Guillaume Bernard <gbernard@koala-lms.org>
2019-05-21 09:50:33 +0200

    fix: do not start container if migration failed

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -7,11 +7,11 @@ if [[ ! -f "${local_settings_file}" ]]; then
     echo "# This file is generated automatically, do not edit manually" > "${local_settings_file}"
 fi
 
-if ! grep SECRET_KEY "${local_settings_file}"; then
+if ! grep SECRET_KEY "${local_settings_file}" &> /dev/null; then
     echo "SECRET_KEY = '$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)'" >> "${local_settings_file}"
 fi
 
-if ! grep ALLOWED_HOSTS "${local_settings_file}"; then
+if ! grep ALLOWED_HOSTS "${local_settings_file}" &> /dev/null; then
     echo "ALLOWED_HOSTS = ['localhost', '127.0.0.1']" >> "${local_settings_file}"
 fi
 
@@ -39,18 +39,23 @@ fi
 # Prepare database migration
 python3 ./manage.py migrate
 
-# Load sample data into database
-if [[ ! -z "${FIXTURE+x}" ]]; then
-    python3 ./manage.py loaddata "${FIXTURE}"
+if [[ $? -eq 0 ]]; then
+    # Load sample data into database
+    if [[ ! -z "${FIXTURE+x}" ]]; then
+        python3 ./manage.py loaddata "${FIXTURE}"
+    else
+        echo "Fixture is not set, skipping loading fixtures…"
+    fi
+
+    # Compile translations
+    python3 ./manage.py compilemessages
+
+    # Collect static files
+    python3 ./manage.py collectstatic --ignore=*.scss
+
+    # Run webserver
+    uwsgi --ini "${uwsgi_file}"
 else
-    echo "Fixture is not set, skipping loading fixtures…"
+    echo "Migration failed, not able to continue"
+    exit 1
 fi
-
-# Compile translations
-python3 ./manage.py compilemessages
-
-# Collect static files
-python3 ./manage.py collectstatic --ignore=*.scss
-
-# Run webserver
-uwsgi --ini "${uwsgi_file}"
