9b8382ba24a7ddd7aab22f4069a9e0bae0db5b7d
Guillaume Bernard <gbernard@koala-lms.org>
2019-06-08 11:01:44 +0200

    fix: ensure all steps run successfully when building docker devel image

--- docker/devel/django-learning.sh
+++ docker/devel/django-learning.sh
@@ -7,31 +7,31 @@ export DJANGO_LEARNING_REQUIREMENTS="jpeg-dev gettext"
 export DJANGO_LEARNING_BUILD_REQUIREMENTS="build-base zlib-dev git sassc"
 
 # Install Alpine requirements
-apk add --no-cache ${DJANGO_LEARNING_REQUIREMENTS} ${DJANGO_LEARNING_BUILD_REQUIREMENTS}
+apk add --no-cache ${DJANGO_LEARNING_REQUIREMENTS} ${DJANGO_LEARNING_BUILD_REQUIREMENTS} && \
 
 # Clone django-koalalms-learning
-git clone "${DJANGO_LEARNING_URL}" "${APPLICATION}" && git -C "${APPLICATION}" checkout develop
+git clone "${DJANGO_LEARNING_URL}" "${APPLICATION}" && git -C "${APPLICATION}" checkout develop && \
 
 # Move to the folder
-cd "${APPLICATION}"
+cd "${APPLICATION}" && \
 
 # Install Python requirements
-pip3 install --no-cache-dir -r requirements.txt
+pip3 install --no-cache-dir -r requirements.txt && \
 
 # Compile translations
-django-admin compilemessages
+django-admin compilemessages && \
 
 # Compile stylesheet
-sassc "learning/static/learning/scss/learning.scss" "learning/static/learning/scss/learning.css"
+sassc "learning/static/learning/scss/learning.scss" "learning/static/learning/scss/learning.css" && \
 
 # Build the Python package
-python3 setup.py sdist bdist_wheel > /dev/null
+python3 setup.py sdist bdist_wheel > /dev/null && \
 
 # Install the package in the system
-pip install --no-deps --force-reinstall dist/*.tar.gz
+pip install --no-deps --force-reinstall dist/*.tar.gz && \
 
 # Remove build artifacts and source code
-cd "${BASE_DIR}" && rm -rf "${APPLICATION}"
+cd "${BASE_DIR}" && rm -rf "${APPLICATION}" && \
 
 # Remove unnecessary packages
 apk del ${DJANGO_LEARNING_BUILD_REQUIREMENTS}
